d277e72146ac72b450cb4d30407c8581fb95c35d
java <java@training.workrocks.ua>
2016-10-31 20:48:48 +0200

    start

---
+++ app/src/main/java/com/levup/notes/NotesActivity.java
@@ -0,0 +1,32 @@
+package com.levup.notes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.levup.notes.adapters.NotesAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotesActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+        recyclerView = (RecyclerView) findViewById(R.id.notes_recycler_view);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        NotesAdapter adapter = new NotesAdapter();
+        List<String> dataSource = new ArrayList<>();
+        for(int i = 0; i < 100; i ++) {
+            dataSource.add("title: " + i);
+        }
+        recyclerView.setAdapter(adapter);
+        adapter.setDataSource(dataSource);
+    }
+}
