1490d9ded5f56057a022316b732308b7e144f79d
java <java@training.workrocks.ua>
2016-11-02 20:49:08 +0200

    added menu

--- app/src/main/java/com/levup/notes/NotesActivity.java
+++ app/src/main/java/com/levup/notes/NotesActivity.java
@@ -1,9 +1,14 @@
 package com.levup.notes;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.levup.notes.adapters.NotesAdapter;
 
@@ -13,11 +18,15 @@ import java.util.List;
 public class NotesActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView = null;
+    private Toolbar toolbar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle(R.string.app_name);
         recyclerView = (RecyclerView) findViewById(R.id.notes_recycler_view);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
         recyclerView.setLayoutManager(layoutManager);
@@ -28,5 +37,31 @@ public class NotesActivity extends AppCompatActivity {
         }
         recyclerView.setAdapter(adapter);
         adapter.setDataSource(dataSource);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.notes_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_settings:
+                Snackbar.make(recyclerView, R.string.settings, Snackbar.LENGTH_LONG).show();
+                return true;
+            case R.id.action_help:
+                Snackbar.make(
+                        recyclerView,
+                        R.string.help,
+                        Snackbar.LENGTH_LONG)
+                        .show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
